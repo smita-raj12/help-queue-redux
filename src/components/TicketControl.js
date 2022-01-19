89ccea7a6264db9e456fe2ed797f1a926c12df01
smita <smita.raj12@gmail.com>
2022-01-18 16:08:01 -0800

    application for help queue with redux

---
+++ src/components/TicketControl.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+import TicketDetail from './TicketDetail';
+import EditTicketForm from './EditTicketForm';
+import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+
+class TicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      selectedTicket: null,
+      editing: false
+    };
+  }
+     
+    handleEditClick = () => {
+      console.log("handleEditClick reached!");
+      this.setState({editing: true});
+    }
+    
+    handleEditingTicketInList = (ticketToEdit) => {
+      const { dispatch } = this.props;
+      const { id, names, location, issue } = ticketToEdit;
+      const action = {
+        type: 'ADD_TICKET',
+        id: id,
+        names: names,
+        location: location,
+        issue: issue,
+      }
+      dispatch(action);
+      this.setState({
+        editing: false,
+        selectedTicket: null
+      });
+    }
+
+    handleChangingSelectedTicket = (id) => {
+      const selectedTicket = this.props.mainTicketList[id];
+      this.setState({selectedTicket: selectedTicket});
+    }
+
+    handleClick = () => {
+      if (this.state.selectedTicket != null) {
+        this.setState({
+          selectedTicket: null,
+          editing: false
+        });
+      } else {
+        const { dispatch } = this.props;
+        const action = {
+          type: 'TOGGLE_FORM'
+        }
+        dispatch(action);
+      }
+    }
+
+    handleDeletingTicket = (id) => {
+      const { dispatch } = this.props;
+      const action = {
+        type: 'DELETE_TICKET',
+        id: id
+      }
+      dispatch(action);
+      this.setState({selectedTicket: null});
+    }
+    
+    handleAddingNewTicketToList = (newTicket) => {
+  const { dispatch } = this.props;
+  const { id, names, location, issue } = newTicket;
+  const action = {
+    type: 'ADD_TICKET',
+    id: id,
+    names: names,
+    location: location,
+    issue: issue,
+  }
+  dispatch(action);
+  const action2 = {
+    type: 'TOGGLE_FORM'
+  }
+  dispatch(action2);
+}
+
+    render(){
+    let currentlyVisibleState = null;
+    let buttonText = null; 
+    if (this.state.editing ) {      
+      currentlyVisibleState = <EditTicketForm ticket = {this.state.selectedTicket} 
+                                    onEditTicket = {this.handleEditingTicketInList} />
+      buttonText = "Return to Ticket List";                              
+    }else if (this.state.selectedTicket != null) {
+      currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket} 
+                                    onClickingDelete = {this.handleDeletingTicket} 
+                                    onClickingEdit = {this.handleEditClick}  
+                                            />
+      buttonText = "Return to Ticket List";
+    }
+    else if (this.props.formVisibleOnPage) {
+      
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList}  />;
+      buttonText = "Return to Ticket List";
+    } else {
+      currentlyVisibleState = <TicketList ticketList={this.props.mainTicketList} onTicketSelection={this.handleChangingSelectedTicket} />;
+      
+      buttonText = "Add Ticket";
+    }
+        return (
+          <React.Fragment>
+            {currentlyVisibleState}
+            <button onClick={this.handleClick}>{buttonText}</button> 
+          </React.Fragment>
+        );
+    }
+}
+
+TicketControl.propTypes = {
+  mainTicketList: PropTypes.object,
+  formVisibleOnPage: PropTypes.bool
+};
+
+const mapStateToProps = state => {
+  return {
+    mainTicketList: state.mainTicketList,
+    formVisibleOnPage: state.formVisibleOnPage
+  }
+}
+
+TicketControl = connect(mapStateToProps)(TicketControl);
+
+export default TicketControl;
---
+++ src/components/TicketList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Ticket from "./Ticket";
+import PropTypes from "prop-types";
+
+function TicketList(props){
+  return (
+    <React.Fragment>
+      <hr />
+     
+      {Object.values(props.ticketList).map((ticket) =>
+        <Ticket
+          whenTicketClicked = { props.onTicketSelection }
+          names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          formattedWaitTime={ticket.formattedWaitTime}
+          id={ticket.id}
+          key={ticket.id}/>
+      )}
+     
+    </React.Fragment>
+  );
+}
+
+
+TicketList.propTypes = {
+  // The PropType below has been updated - it's now an object, not an array.
+  ticketList: PropTypes.object,
+  onTicketSelection: PropTypes.func
+};
+export default TicketList;
